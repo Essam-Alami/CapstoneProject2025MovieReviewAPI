9388fddae17eb51747b5e394ef06862f5494339a
VampyrFangs <testezmonclic@gmail.com>
2025-09-30 16:51:09 +0100

    ....

--- moviereviewproject/api/views.py
+++ moviereviewproject/api/views.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse
 from .models import Movie, Review
 import requests
 from .models import Movie, Review
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
@@ -14,6 +15,11 @@ class MovieViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Assign the logged-in user automatically
+        serializer.save(user=self.request.user)
 
 
 def home(request):
