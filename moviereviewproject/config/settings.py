7aee9501c35846b3bb2603e3ffd59673e6d4f069
VampyrFangs <testezmonclic@gmail.com>
2025-10-09 16:01:47 +0100

    Made changes to the repo.

--- moviereviewproject/config/settings.py
+++ moviereviewproject/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4pk#)g-7_5qf)bd3uwnwqa6^n%c3$9yjsj-@_8z#lzp$_w%+_t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Patroclus22.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
